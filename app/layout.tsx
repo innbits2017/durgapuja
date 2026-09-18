7a1e010cd23d823f91b583a44c81d07d9890eeec
innbits2017 <innbits@gmail.com>
2026-09-18 09:14:33 +0530

    googleanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const openSans = Open_Sans({
   subsets: ["latin"],
@@ -17,7 +18,20 @@ export default function RootLayout({
         <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-      />
+        />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9MFCEK6DWD"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9MFCEK6DWD');
+          `}
+        </Script>
       </head>
       <body className={openSans.className}>
         {children}
